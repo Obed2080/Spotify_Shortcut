8d17a001e6cd4d4520e776a42e543f1499cccd8c
Obed <espinozaobed10@gmail.com>
2025-07-11 23:51:58 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def save_shortcut():
+
+    playlist = "The One"
+
+    key_combination = "Ctrl+Shift+S"
+
+    # get the current song from the player
+    current_song = get_current_song()
+
+    # if the song is already in the playlist, do not add it again
+
+
+    # if the song is not in the playlist, add it to the playlist
+
+save_shortcut
